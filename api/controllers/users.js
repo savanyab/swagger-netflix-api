f8e61e79878113d9667ccf365521a725cf63cf4f
Savanya Brigitta <brigitta.savanya.js@gmail.com>
2019-07-26 20:39:41 +0200

    add login method + randomstring

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,16 +1,38 @@
+const randomstring = require('randomstring');
+
 module.exports = {
-  createUser: createUser
+  createUser: createUser,
+  login: login
 }
+
 
 function createUser(req, res) {
   const email = req.swagger.params.user.value.email;
   const password = req.swagger.params.user.value.password;
-  
+
   const collection = req.app.locals.usersCollection;
   collection.insert({ "email": email, "password": password });
   collection.find({ "email": email }).toArray()
     .then(newUser => {
       res.status(201);
       res.json({ "id": newUser[0]._id, "email": newUser[0].email, "password": "Saved" });
+    }).catch(e => res.send(e.message));
+}
+
+function login(req, res) {
+  const email = req.swagger.params.credentials.value.email;
+  const password = req.swagger.params.credentials.value.password;
+  const collection = req.app.locals.usersCollection;
+  const sessionId = randomstring.generate(32);
+
+  collection.find({ "email": email }).toArray()
+    .then(user => {
+      if (!user[0] || user[0].password !== password) {
+        return res.status(401).json({message: "Invalid email or password"})
+      }
+      res.json({ "sessionID": sessionId });
+    }).catch(e => {
+      res.status(401);
+      res.send(e.message);
     });
 }
