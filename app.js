04a5652348f04092a126220597eaaa3be4c1f826
Savanya Brigitta <brigitta.savanya.js@gmail.com>
2019-07-26 19:36:45 +0200

    set up db connection in app.js

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var SwaggerExpress = require('swagger-express-mw');
 var app = require('express')();
 module.exports = app; // for testing
 
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost';
+
 var config = {
   appRoot: __dirname // required config
 };
@@ -14,9 +17,19 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
   // install middleware
   swaggerExpress.register(app);
 
-  var port = process.env.PORT || 10010;
-  app.listen(port);
-
+  try {
+    MongoClient.connect(url, (err, client) => {
+      const db = client.db('netflix');
+      const usersCollection = db.collection('users');
+      const videosCollection = db.collection('videos');
+      const port = process.env.PORT || 10010;
+      app.locals.usersCollection = usersCollection;
+      app.locals.videosCollection = videosCollection;
+      app.listen(port);      
+    });
+  } catch (err) {
+    console.log(err);
+  }
   if (swaggerExpress.runner.swagger.paths['/hello']) {
     console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
   }
