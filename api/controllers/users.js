dfc2251f626067321e501506d79548150a77df75
Savanya Brigitta <brigitta.savanya.js@gmail.com>
2019-07-30 16:52:04 +0200

    rename variable, move second query to then()

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -10,25 +10,26 @@ function createUser(req, res) {
   const email = req.swagger.params.user.value.email;
   const password = req.swagger.params.user.value.password;
 
-  const collection = req.app.locals.usersCollection;
-  collection.insert({ "email": email, "password": password });
-  collection.find({ "email": email }).toArray()
-    .then(newUser => {
-      res.status(201);
-      res.json({ "id": newUser[0]._id, "email": newUser[0].email, "password": "Saved" });
-    }).catch(e => res.send(e.message));
+  const usersCollection = req.app.locals.usersCollection;
+  usersCollection.insert({ "email": email, "password": password }).then(
+    usersCollection.find({ "email": email }).toArray()
+      .then(newUser => {
+        res.status(201);
+        res.json({ "id": newUser[0]._id, "email": newUser[0].email, "password": "Saved" });
+      }))
+    .catch(e => res.send(e.message));
 }
 
 function login(req, res) {
   const email = req.swagger.params.credentials.value.email;
   const password = req.swagger.params.credentials.value.password;
-  const collection = req.app.locals.usersCollection;
+  const usersCollection = req.app.locals.usersCollection;
   const sessionId = randomstring.generate(32);
 
-  collection.find({ "email": email }).toArray()
+  usersCollection.find({ "email": email }).toArray()
     .then(user => {
       if (!user[0] || user[0].password !== password) {
-        return res.status(401).json({message: "Invalid email or password"})
+        return res.status(401).json({ message: "Invalid email or password" })
       }
       res.json({ "sessionID": sessionId });
     }).catch(e => {
